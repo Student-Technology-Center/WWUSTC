c2b4e456b4f18ec325bd8e3232de675083aa15d1
S-Toad <alexayala9975@hotmail.com>
2017-06-16 00:54:01 +0000

    Improve update script

--- update.sh
+++ update.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-echo "Cloning Django-Cas"
-git clone https://github.com/kstateome/django-cas.git
+if [ ! -d cas/ ]
+then
+  echo "Cloning Django-Cas"
+  git clone https://github.com/kstateome/django-cas.git
+  mv django-cas/cas/ cas/
+  rm -rf django-cas/
+fi
 
-echo "Cloning Hour Manager"
-git clone https://github.com/Student-Technology-Center/Hour-Manager.git
+if [ ! -d hour_manager/ ]
+then
+  echo "Cloning Hour Manager"
+  git clone https://github.com/Student-Technology-Center/Hour-Manager.git
+  mv Hour-Manager/ hour_manager/
+else
+  echo "Updating Hour-Manager..."
+  cd hour_manager/
+  git pull
+  cd ..
+fi
