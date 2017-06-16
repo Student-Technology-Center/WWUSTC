ae2e812bf6f932612149517f2f29d505044673d4
S-Toad <alexayala9975@hotmail.com>
2017-06-16 00:23:13 +0000

    Add update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Cloning Django-Cas"
+git clone https://github.com/kstateome/django-cas.git
+
+echo "Cloning Hour Manager"
+git clone https://github.com/Student-Technology-Center/Hour-Manager.git
