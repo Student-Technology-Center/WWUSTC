9211bc193febab37ada049bb36460a2c1e709412
Alex Ayala <AlexAyala9975@hotmail.com>
2017-07-03 12:49:05 -0700

    Modify start script to take in a IP and port

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+serverIP=$1
+port=$2
+
+if [[ $2 -eq 0 ]] ; then
+  echo "Usage: ./start.sh <IP> <Port>"
+  exit 1
+fi
+
 while :
 do
-	python3 manage.py runserver wwustc.com:8000
+	python3 manage.py runserver $1:$2
 	echo Restarting in 5 Seconds, Ctrl+C to cancel...
 	sleep 1
 	
