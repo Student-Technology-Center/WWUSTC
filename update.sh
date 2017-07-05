956c8a7fad8df3b631a7f29e224f142db9efa785
Alex Ayala <AlexAyala9975@hotmail.com>
2017-07-05 12:47:28 -0700

    Modify update script
    It will now only restart the django server if the main repo was modified.
    The main repo is the only repo it checks to see the status of.

--- update.sh
+++ update.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
-update=false
 
 apps=(
   hour_manager 
   lfp_scheduler 
 )
+
+check-repo ()
+{
+  git remote update
+  
+  UPSTREAM=${1:-'@{u}'}
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse "$UPSTREAM")
+  BASE=$(git merge-base @ "$UPSTREAM")
+
+  if [[ $LOCAL = $REMOTE ]]; then echo "Up-to-date";
+  elif [[ $LOCAL = $BASE ]]; then 
+    echo "Update found, staging to update"; 
+    git pull;
+    tmux send-keys -t stc:2 C-c && (
+      echo "Succesfully told django to restart"
+    ) || (
+      echo "Tmux wasn't found, this is no problemo"
+    )
+  elif [[ $REMOTE = $BASE ]]; then echo "Local files have been edited.";
+  else echo "Diverged";
+  fi
+}
 
 ## The cas check is temporary code, if CAS is to be removed, this check will be removed.
 ## If cas was to be used, we would need to implement CAS as a git submodule
@@ -16,65 +38,14 @@ then
   rm -rf django-cas/
 fi
 
-check-repo ()
-{
-  git remote update
-  
-  UPSTREAM=${1:-'@{u}'}
-  LOCAL=$(git rev-parse @)
-  REMOTE=$(git rev-parse "$UPSTREAM")
-  BASE=$(git merge-base @ "$UPSTREAM")
-
-  if [ $LOCAL = $REMOTE ]; then
-      echo "Up-to-date"
-  elif [ $LOCAL = $BASE ]; then
-      echo "Update found, staging to update"
-      if $1
-      then
-        git pull
-      else
-        update=true
-      fi
-  elif [ $REMOTE = $BASE ]; then
-      echo "Local files have been edited."
-  else
-      echo "Diverged"
-  fi
-}
-
-
-
 for item in ${apps[*]}
 do
   printf "Cheking %s\n" $item
-  if [ "$(ls -A $item)" ]; then
-    # Not Empty
-    cd $item/
-    check-repo
-    cd ..
-    
-    if $update; then break; fi
-  else
-    # Empty Folder
-    # If there's an empty folder, we initialize the submodules and update regardless of the state of the other items.
+  if ! [[ "$(ls -A $item)" ]]; then
     git submodule init
-    git submodule update
-    exit 
   fi
 done
 
-# Checks the main directory
-check-repo true
-
-if $update
-then
-  echo "Updating Repos"
-  git submodule update
-  
-  echo "Attempting to send tmux command"
-  tmux send-keys -t stc:2 C-c && (
-    echo "Succesfully told django to restart"
-  ) || (
-    echo "Tmux wasn't found, this is no problemo"
-  )
-fi
+echo "Updating Repos"
+git submodule update
+check-repo
