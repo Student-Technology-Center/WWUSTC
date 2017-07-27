825a186fdb9389d0048a01fde4a3461bc9721e64
Christian <christianbrintnall@gmail.com>
2017-07-27 15:41:13 -0700

    Add issue tracker

--- index/views.py
+++ index/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
 
     context = {
         "user":request.user,
     }
+
+    context['issues'] = requests.get('https://api.github.com/repos/Student-Technology-Center/WWUSTC/issues').json()
 
     return render(
         request,
