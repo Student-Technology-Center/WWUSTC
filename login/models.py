da029789eaf757457b305bbd930e377b1513cd51
Christian <christianbrintnall@gmail.com>
2017-07-28 11:08:03 -0700

    Final test commit

--- login/models.py
+++ login/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.forms import ModelForm
 
-class UserOptions(models.Model):
-    email = models.BooleanField(default=False)
-    texting = models.BooleanField(default=False)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-
-    objects = UserOptionsManager()
-
 class UserOptionsManager(models.Manager):
     def create_options(self, user):
             options = self.create(user)
@@ -16,6 +9,14 @@ class UserOptionsManager(models.Manager):
             options.email = False
             options.save()
             return options
+
+class UserOptions(models.Model):
+    email = models.BooleanField(default=False)
+    texting = models.BooleanField(default=False)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    objects = UserOptionsManager()
+
 
 class UserOptionsForm(ModelForm):
     class Meta:
