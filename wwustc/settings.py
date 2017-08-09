be94a0d5b878a702355a17634e43f6fe9d1fd205
Christian <christianbrintnall@gmail.com>
2017-08-09 14:49:52 -0700

    Redo index css. Add static location fix

--- wwustc/settings.py
+++ wwustc/settings.py
@@ -137,13 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/hour_manager/static',
-    '/wiki/static',
-    '/lfp_scheduler/static',
+    "/var/www/wwustc/static",
 ]
 
 SITE_ID = 1
