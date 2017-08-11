45c05db8a07a11d598b677ffd49d4eb09554717c
Alex Ayala <AlexAyala9975@hotmail.com>
2017-08-11 14:20:50 -0700

    Remove submodules

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Simply clones git repos
+
+git clone https://github.com/Student-Technology-Center/django-wiki.git django-wiki
+git clone https://github.com/Student-Technology-Center/Hour-Manager.git hour_manager
+git clone https://github.com/Student-Technology-Center/evaluations.git evaluations
+git clone https://github.com/Student-Technology-Center/lfp_scheduler.git lfp_scheduler
