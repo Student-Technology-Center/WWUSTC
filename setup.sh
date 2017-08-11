73c4740a720db5b8d8461703c1bd6c33f7fd6174
S-Toad <alexayala9975@hotmail.com>
2017-08-11 14:48:47 -0700

    Add migrations to setup script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ git clone https://github.com/Student-Technology-Center/django-wiki.git django-wi
 git clone https://github.com/Student-Technology-Center/Hour-Manager.git hour_manager
 git clone https://github.com/Student-Technology-Center/evaluations.git evaluations
 git clone https://github.com/Student-Technology-Center/lfp_scheduler.git lfp_scheduler
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo 'yes' | python3 manage.py collectstatic
+
