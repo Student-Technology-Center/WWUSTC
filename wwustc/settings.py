1727dae68e049835ad676c56fec4a01b96464492
S-Toad <alexayala9975@hotmail.com>
2017-08-12 17:18:01 -0700

    Set static and medias root to relative dir

--- wwustc/settings.py
+++ wwustc/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/wwustc/static'
+STATIC_ROOT = 'static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/wwustc/media'
+MEDIA_ROOT = 'media/'
 
 SITE_ID = 1
 
