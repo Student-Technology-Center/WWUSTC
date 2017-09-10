8e06390f868784a99e0a62db4302f33a08e9eb11
Christian <christianbrintnall@gmail.com>
2017-09-09 22:16:17 -0700

    Integrate shift manager into index, add super user checking

--- index/views.py
+++ index/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 import requests
 
 def index(request):
+
+    '''
+    Below is where we define who will become an admin, as the index will check.
+    '''
+
+    admin_usernames = [
+        'brintnc',
+        'test' #TODO: REMOVE THIS <---
+    ]
+
+    user_model = get_user_model()
+
+    for name in admin_usernames:
+        try:
+            user = user_model.objects.get(username=name)
+        except:
+            break
+        user.is_superuser = True
+        user.save()
 
     context = {
         "user":request.user,
