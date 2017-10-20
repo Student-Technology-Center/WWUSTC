a1ca560c0429b6539f96f042537e7d7dce42b482
Christian <christianbrintnall@gmail.com>
2017-10-20 16:12:10 -0700

    Add favicon, konami code, and start server script

--- index/static/js/index_main.js
+++ index/static/js/index_main.js
@@ -1,8 +1,34 @@
 var currentInfo;
 
+//num is the limit.
+function getRandomWholeNumber(num) {
+    return Math.ceil(Math.random() * num)
+}
+
+var rotation_cap = 360;
+var movement_cap = 300;
+var scale_cap = 2;
+
 $(document).ready(function() {
     $('.app_button').mouseenter(function() {
         currentInfo = $(this).children()[0].children[2].innerHTML;
         $('#app_info').text(currentInfo);
     })
+    $( window ).konami({
+		cheat: function() {
+			$('.app_link').each(function (i, obj){
+                $(this).css({
+                    "background-color": randRGB(),
+                }, 1000);
+
+                var rotationAmt = getRandomWholeNumber(rotation_cap).toString() + "deg";
+                var moveAmt = getRandomWholeNumber(movement_cap).toString() + "px";
+                var scaleAmt = getRandomWholeNumber(scale_cap).toString();
+
+                $(this).parent().css({
+                    "transform": "rotate(" + rotationAmt + ") translateX(" + moveAmt + ") translateY(" + moveAmt + ") scale(" + scaleAmt + ")"
+                }, 1000);
+            });
+		}
+	});
 });
---
+++ utils/scripts/start_server.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process
+import os
+import subprocess
+
+def main():
+    print("Beginning Django server")
+
+    #Begins the server process
+    p = Process(target=launcher_server)
+    p.start()
+    p.join()
+
+def launcher_server():
+
+    py_commands = [
+        'py',
+        'python',
+        'python3'
+    ]
+
+    os.chdir('../../')
+
+    for cmd in py_commands:
+        subprocess.call('{} manage.py runserver --settings=wwustc.dev-settings'.format(cmd), shell=True)
+
+if __name__ == '__main__':
+    main()
