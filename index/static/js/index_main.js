dad87efab00565e03efe8cdef4cb0e73767de24e
Christian <christianbrintnall@gmail.com>
2018-03-08 12:31:09 -0800

    Move icons up to top in preparation of shifts

--- index/static/js/index_main.js
+++ index/static/js/index_main.js
@@ -38,20 +38,52 @@ $(document).ready(function() {
 function getTodaysShifts() {
 
     var today = moment().format('YYYY-MM-DD')
-    var time = moment().format('HH:mm')
-
-    console.log(time)
+    var shifts = [];
 
     $.ajax({
         url: '/shifts/api/get_shifts/?start=' + today + '&end=' + today,
         fail: function () {
-            setError("Something went wrong - file a bug report. Please retry the submission");
+            console.log("Couldn't get shifts.")
         },
         success: function(data) {
             data.success.forEach(function(e) {
-                console.log(e)
+                var user = {}
+                
+                user.time = moment(e.datetime)
+                user.name = e.name;
+                user.up = e.up_for_grabs;
+
+                shifts.push(user);
             })
+
+            combineShifts(shifts)
         }
     })
+}
+
+function combineShifts(shifts) {
+
+    var combined_shifts = [];
+
+    shifts.forEach(function (shift) {
+        var end = new moment(shift.time);
+        end.add(1, 'hours');
+        var shift_pool = [];
+
+        shifts.forEach(function (nextShift) {
+
+            if (nextShift.time.format('h:mm a') === end.format('h:mm a') && 
+                nextShift.name === shift.name) {
+                end.add(1, 'hours');
+                shift_pool.push(nextShift);
+            }
+        })
+
+        if (shift_pool.length >= 0) {
+            combined_shifts.push(shift_pool);
+        }
+    })
+
+    console.log(combined_shifts);
 
 }
