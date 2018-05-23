7d6c6f465bcbb8063c73483b120004611b3cdb84
nick <n.monkey.d@gmail.com>
2018-05-23 00:05:32 -0700

    Updated dev settings to prevent server from caching templates, since that gets really, really annoying

--- wwustc/dev-settings.py
+++ wwustc/dev-settings.py
@@ -6,3 +6,25 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1" 
 ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': DEBUG,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'sekizai.context_processors.sekizai',
+            ],
+        },
+    },
+]
+
+ADMIN_LIST += [
+    'deyounn',
+]
