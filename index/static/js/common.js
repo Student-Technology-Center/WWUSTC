8295b32ef19d5e633464e13077dd8a500b6899da
Christian <christianbrintnall@gmail.com>
2018-05-30 20:51:24 -0700

    Add helper functions for ajax requests, remove middleware (not needed)

--- index/static/js/common.js
+++ index/static/js/common.js
@@ -17,24 +17,6 @@ function getTodaysDate() {
 	return (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear();
 }
 
-function z(url, data) {
-	$.ajax({
-		url		: url,
-		data	: data,
-		type	: 'POST',
-		headers	: { 'methodoverride' : 'DELETE' }
-	})
-}
-
-function ajax_post(url, data, success) {
-	$.ajax({
-		url		: url,
-		data	: data,
-		type	: 'POST',
-		success	: success
-	})
-}
-
 function ajax_get(url, data, success) {
 	$.ajax({
 		url		: url,
@@ -44,24 +26,63 @@ function ajax_get(url, data, success) {
 	})
 }
 
-/* Override the POST type with PUT */
-function ajax_put(url, data, success) {
-	$.ajax({
-		url		: url,
-		data	: data,
-		type	: 'POST', //This is intentional.
-		success	: success,
-		headers	: { 'methodoverride' : 'PUT' }
-	})
-}
-
-/* Override the POST type with DELETE */
-function ajax_delete(url, data, success) {
+function ajax_post(url, data, success) {
 	$.ajax({
 		url		: url,
 		data	: data,
 		type	: 'POST',
 		success	: success,
-		headers	: { 'methodoverride': 'DELETE' }
+		headers: {
+			'X-CSRFToken' : getCookie('csrftoken')
+		}
 	})
+}
+
+function ajax_put(url, data, success) {
+	$.ajax({
+		url		: url,
+		data	: data,
+		type	: 'PUT',
+		success	: success,
+		headers: {
+			'X-CSRFToken' : getCookie('csrftoken')
+		}
+	})
+}
+
+function ajax_delete(url, data, success) {
+	$.ajax({
+		url		: url,
+		data	: data,
+		type	: 'DELETE',
+		success	: success,
+		headers: {
+			'X-CSRFToken' : getCookie('csrftoken')
+		}
+	})
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
 }
