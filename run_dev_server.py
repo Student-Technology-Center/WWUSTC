84c4b981e87551cbc8045f7d88f3dfbfc9527970
Jagannath Natarajan <jagnat4000@gmail.com>
2018-08-06 11:50:37 -0700

    fix run_dev_server.py - why were we joining a path with itself?!

--- run_dev_server.py
+++ run_dev_server.py
@@ -5,10 +5,11 @@ import os
 
 # Get path of this file
 basePath = os.path.dirname(os.path.abspath(__file__))
-managePath = os.path.join(basePath, basePath)
+managePath = os.path.join(basePath, "manage.py")
 
 check_call([
     sys.executable,
     managePath,
+    "runserver",
     "--settings=wwustc.dev-settings"
 ])
